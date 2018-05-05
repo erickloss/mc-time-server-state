bb409421ae8ba05677eef01e78ea03eac55b6775
Eric Kloss <eric.kloss@sandstorm.de>
2018-05-05 11:44:15 +0200

    lags only in main base chunk

--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,2 @@
-java -Xmx6g -Xms256m -XX:PermSize=256m -Dfml.queryResult=confirm -jar ./forge-1.12.2-14.23.3.2655-universal.jar nogui
+java -server -Xmx6g -Xms256m -XX:PermSize=256m -Dfml.queryResult=confirm -jar ./forge-1.12.2-14.23.3.2655-universal.jar nogui
 
